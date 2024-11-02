7dcaf5713bb08913de62f6d7a427a0744401dffa
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-11-02 17:40:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,25 @@ import Share from "./components/share/Share";
 import "./App.css";
 
 function App() {
+   const token = localStorage.getItem("token");
+
    return (
       <BrowserRouter>
          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/share/:projectId" element={<Share />} />
+            {token ? (
+               <>
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/" element={<Navigate to="/home" replace />} />
+               </>
+            ) : (
+               <>
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/signin" element={<Signin />} />
+                  <Route path="*" element={<Navigate to="/signin" replace />} />
+                  <Route path="/" element={<Navigate to="/signin" replace />} />
+                  <Route path="/share/:projectId" element={<Share />} />
+               </>
+            )}
          </Routes>
       </BrowserRouter>
    );
